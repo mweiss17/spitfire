79da321a54fee7df70744a9871c29bd7ea262a8a
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-02-21 15:18:48 -0500

    we liiiiiiiiiiiive

--- spitfire/urls.py
+++ spitfire/urls.py
@@ -1,3 +1,4 @@
+from spitfire import views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -10,6 +11,6 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
