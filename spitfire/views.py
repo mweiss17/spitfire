68be80bc74d2ebe2b1728eebf61939f9bca13344
mweiss17 <martin.weiss@mail.mcgill.ca>
2015-02-21 17:16:03 -0500

    shitty views

--- spitfire/views.py
+++ spitfire/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
- 
+from search_youtube.py import youtube_search
+
 def index(request):
     context = {}
+    results = search_youtube(request.get['query_name_orwe'])
+    
     return render(request, 'index.html', context)
