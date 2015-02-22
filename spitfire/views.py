b3bcfde78041e71a9042de338b3c604ee943221f
davidhernon <david.hernon@mail.mcgill.ca>
2015-02-21 19:36:00 -0500

    working with bieber

--- spitfire/views.py
+++ spitfire/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from spitfire.forms import *
 
 def index(request):
-    context = {}
-    #results = search_youtube(request.get['query_name_orwe'])
-    search_form = search_spitfire_form(request.POST)
-    context.update({"search_form":search_form})
-    if search_form.is_valid(): 
-	return render(request, 'valid.html', context)
-    return render(request, 'index.html', context)
+	context = {}
+	if request.method == "POST":
+		search_form = search_spitfire_form(request.POST)
+		if search_form.is_valid():
+			query = search_form.cleaned_data["search_query"]
+			context.update({"query":query})
+			return render(request, 'other.html', context)	
+	search_form = search_spitfire_form()	
+	context.update({"search_form":search_form})
+	return render(request, 'index.html', context)
+
+def other(request):
+	context = {}
+	return render(request, 'other.html', context)
+
