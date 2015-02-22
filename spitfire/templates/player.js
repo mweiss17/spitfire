50e60cd84ac93d658113ca405fa032bbaf55869b
davidhernon <david.hernon@mail.mcgill.ca>
2015-02-22 01:58:37 -0500

    fixed player

---
+++ spitfire/templates/player.js
@@ -0,0 +1,52 @@
+// Create an iFrame and load the player
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // After API code downloads above, display the iFrame and generate list of songs from the query
+      var songs_from_query = [];
+    //Create a list of songs from django
+    {% for title in track_titles %}
+		{% for code in result_codes %}
+			{% if forloop.counter == forloop.parentloop.counter %}
+				songs_from_query.push({track: "{{title}}", url: "{{code}}"});
+			{% endif %}
+		{% endfor %}
+	{% endfor %}
+
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: songs_from_query[0].url,
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      player.startVideo();
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        //event.target.playVideo();
+      }
+
+      //  The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        // if (event.data == YT.PlayerState.PLAYING && !done) {
+        //   //setTimeout(stopVideo, 6000);
+        //   done = true;
+        // }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
